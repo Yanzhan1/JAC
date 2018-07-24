2cf469a715d0c50edf7db3fde38749829c37a69e
fangtao.yu <342285154@qq.com>
2018-07-24 11:33:22 +0800

    切换频道

--- src/components/Wit/Recoment_bus.vue
+++ src/components/Wit/Recoment_bus.vue
@@ -1,46 +1,47 @@
 <template>
-    <div>
-        <header class="header">
-            <img class="header-left" :src="'./static/images/back@2x.png'" @click="$router.go(-1)">
-            <div id="tip">
-                <span class="header-title active" style="margin-right:.2rem">全部车型</span>
-                <span class="header-title">主推车型</span>
-            </div>
-            <span @click="shai()" class="header-right"><img src="../../../static/images/Wit/shaixuan1@3x.png" alt="" style="width:.4rem"></span>
-        </header>
-        <div style="height:.88rem"></div>
-        <ul>
-            <li class="bus_li" v-for="(item,index) in 5" :key="index">
-                <img src="../../../static/images/Wit/bg-mine.png" alt="">
-                <div class="bus_1">
-                    <span class="bus_2">瑞丰S7</span>
-                    <span class="bus_3">
-                        <span style="color:#a5a5a5;font-size:.22rem"> 官方指导价</span> ：9.7万起</span>
-                </div>
-                <img src="../../../static/images/next@2x.png" alt="" style="width:.4rem;height:.4rem">
-            </li>
-        </ul>
-        <mt-popup v-model="popupVisible" position="middle">
-            <div style="width:100%;text-align:center;line-height:.88rem;font-size:.36rem;color:#555555;border-bottom:1px solid #f1f1f1">
-                切换频道
-            </div>
-            <div class="flex" style="margin-top:.3rem">
-                <label class="input-label" :class="{active: selected_all}" @click="slect_all"></label>
-                <span class="txt" style="margin-left:.1rem">全选</span>
-            </div>
-            <ul class="flex row wrap ">
-                <li class="flex row list_li cocenter" v-for="(item,index) in good_list" :key="index">
-                    <label class="input-label" :class="{active: item.is_selected}" @click="select_one(index)"></label>
-                    <span class="txt" style="margin-left:.1rem">{{item.name}}</span>
-                </li>
+  <div>
+    <header class="header">
+      <img class="header-left" :src="'./static/images/back@2x.png'" @click="$router.go(-1)">
+      <div id="tip">
+        <span class="header-title active" style="margin-right:.2rem">全部车型</span>
+        <span class="header-title">主推车型</span>
+      </div>
+      <span @click="shai()" class="header-right"><img src="../../../static/images/Wit/shaixuan1@3x.png" alt="" style="width:.4rem"></span>
+    </header>
+    <div style="height:.88rem"></div>
+    <ul>
+      <li class="bus_li" v-for="(item,index) in 5" :key="index">
+        <img src="../../../static/images/Wit/bg-mine.png" alt="">
+        <div class="bus_1">
+          <span class="bus_2">瑞丰S7</span>
+          <span class="bus_3">
+            <span style="color:#a5a5a5;font-size:.22rem"> 官方指导价</span> ：9.7万起</span>
+        </div>
+        <img src="../../../static/images/next@2x.png" alt="" style="width:.4rem;height:.4rem">
+      </li>
+    </ul>
+    <mt-popup v-model="popupVisible" position="middle">
+      <div style="width:100%;text-align:center;line-height:.88rem;font-size:.36rem;color:#555555;border-bottom:1px solid #f1f1f1">
+        切换频道
+      </div>
+      <div class="flex row cocenter roe">
+        <label class="input-label" :class="{active: selected_all}" @click="slect_all"></label>
+        <span class="txt" style="margin-left:.1rem">全选</span>
+      </div>
+      <ul class="flex row wrap between">
+        <!-- flex row list_li cocenter -->
+        <li class="flex row cocenter list_li" v-for="(item,index) in good_list" :key="index">
+          <label class="input-label" :class="{active: item.is_selected}" @click="select_one(index)"></label>
+          <span class="txt" style="margin-left:.1rem">{{item.name}}</span>
+        </li>
 
-            </ul>
-            <div style="width:100%;display:flex;text-align:center;padding:.3rem">
-                <p style="felx:1;width:100%;font-size:.32rem;color:#888888;">取消</p>
-                <p style="felx:1;width:100%;font-size:.32rem;color:#49BBFF;">确定</p>
-            </div>
-        </mt-popup>
-    </div>
+      </ul>
+      <div class="fot" style="width:100%;display:flex;text-align:center;padding:.3rem">
+        <p class="pp" style="felx:1;width:100%;font-size:.32rem;color:#888888;" @click="fn">取消</p>
+        <p style="felx:1;width:100%;font-size:.32rem;color:#49BBFF;" @click="fn">确定</p>
+      </div>
+    </mt-popup>
+  </div>
 </template>
 <script>
 import { Popup } from "mint-ui";
@@ -64,7 +65,7 @@ export default {
     shai() {
       this.popupVisible = true;
     },
-     select_one(index) {
+    select_one(index) {
       if (this.good_list[index].is_selected == true) {
         this.good_list[index].is_selected = false;
       } else {
@@ -83,6 +84,9 @@ export default {
         }
         this.selected_all = true;
       }
+    },
+    fn() {
+      this.popupVisible = false;
     }
   }
 };
@@ -99,20 +103,41 @@ $(function() {
 .mint-popup {
   width: 80%;
 }
+.fot{
+position: relative;
+}
+.pp:after {
+  content: "";
+  position: absolute;
+ right: 3rem;
+    top: .2rem;
+  width: 0.01rem;
+  height: 0.6rem;
+  background-color: #f1f1f1;
+}
 .row {
   flex-direction: row;
 }
-.wrap{
-    flex-wrap: wrap
+.wrap {
+  flex-wrap: wrap;
+  border-bottom: 1px solid #f1f1f1;
+}
+.wrap li:nth-of-type(odd) {
+  margin-left: 0.8rem;
+}
+.roe {
+  margin-left: 0.8rem;
+  line-height: 0.88rem;
 }
 .active {
   color: #ccc;
 }
-.list_li{
-    width:50%;
-    line-height: .88rem;
-   
+.list_li {
+  width: 40%;
+  line-height: 0.88rem;
+  vertical-align: middle;
 }
+
 .bus_li {
   height: 2.3rem;
   border-bottom: 1px solid #f5f5f5;
