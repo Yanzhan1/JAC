bbb7592fc1a24dc39e734b93a9fa6d4fc74ec0da
于成龙 <chenglong.yu@timanetworks.com>
2018-08-09 11:41:14 +0800

    【修改】uid不许写死

--- src/store/state.js
+++ src/store/state.js
@@ -1,6 +1,7 @@
 export default {
-    // islogin: false, //判断是否登录 正式上线用
-    islogin: true, //判断是否登录 测试用
-  userId:1165864,//用户userID 正式上线用
+    islogin: false, //判断是否登录 正式上线用
+    // islogin: true, //判断是否登录 测试用
+  // userId:1165864,//用户userID 正式上线用
+  userId:null,
   UserStartId:null,
 }
