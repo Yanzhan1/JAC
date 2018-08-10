0cc9f222b70cf88897cc23ca9b2bb26a6a8b8544
于成龙 <chenglong.yu@timanetworks.com>
2018-08-10 15:22:46 +0800

    【修改】加两个弹窗验证一下

--- src/App.vue
+++ src/App.vue
@@ -19,10 +19,13 @@ export default {
         this.$store.dispatch('isLogin',true);
         this.$store.dispatch('userId',userInfo.userId);
         this.$store.dispatch('uuid',userInfo.uuid);
+        alert("登录了！接收到:"+userInfo.userId+userInfo.mobile+"vuex:"+this.$store.state.isLogin+this.$store.state.userId)
       }else{
         this.$store.dispatch('isLogin',false);
         this.$store.dispatch('userId',null);
         this.$store.dispatch('uuid',null);
+        alert("登出了！接收到:"+userInfo+"vuex:"+this.$store.state.isLogin+this.$store.state.userId)
+
       }
     },
   	getNo () { //调试使用的模拟no
