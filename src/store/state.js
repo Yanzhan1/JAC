b4bc1110e35a691269e17b475f505d03efcf4c0b
Xu Tingting <tingting.xu@timanetworks.com>
2018-08-28 17:19:17 +0800

    我的发布html

--- src/store/state.js
+++ src/store/state.js
@@ -20,6 +20,6 @@ export default {
             "timaToken": "Tima eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySW5mbyI6IntcImF1dGhlbnRpY2F0aW9uU3RhdHVzXCI6MCxcImlkXCI6NjcsXCJpbml0VXNlclwiOjAsXCJub1wiOlwiQUQwMjIwMTgwODI3MDE1OTUwMjAyOTNcIixcInBob25lXCI6XCIxNTM2Mjg1NjM0NVwiLFwidXNlclN0YXR1c1wiOjAsXCJ1c2VyVHlwZVwiOlwiMDFcIn0iLCJjcmVhdGVkIjoxNTM1MzQ5Njg4MjMyLCJ1c2VyTm8iOiJBRDAyMjAxODA4MjcwMTU5NTAyMDI5MyIsInVzZXJUeXBlIjoiMDEiLCJleHAiOjE1MzYyMTM2ODgsInVzZXJJZCI6Njd9.9VWyLG7c5fHw-nsxSEunHUVjDVu4kiw_9FE_0FMcmrA"
         }
     }, //除了爱车部分暂用的token
-    vins: '', //车辆的pin码暂时用，登入后拿到放这里 
+    vins: '', //车辆的pin码暂时用，登入后拿到放这里
     loadingflag: false, //loading动画状态
 }
