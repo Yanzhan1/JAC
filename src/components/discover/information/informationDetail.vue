a3012d7c1e5c7b1bec23c33527debf1cddc1c40d
David <>
2018-09-06 14:37:25 +0800

    1471 测试环境：app 资讯详情界面下发评论，评论区不显示用户头像，先集用户头像，界面一片空白 更改逻辑 并修改路径

--- src/components/discover/information/informationDetail.vue
+++ src/components/discover/information/informationDetail.vue
@@ -4,18 +4,18 @@
     <div>
       <header class="header0 header1" id="header1">
         <img class="header_left" src="../../../../static/images/discover/backfff.png" @click="goBack">
-        <img class="header_right" src="../../../../static/images/discover/morefff.png" @click="onShareClick(0)"/>
+        <img class="header_right" src="../../../../static/images/discover/morefff.png" @click="onShareClick(0)" />
       </header>
       <header class="header0 header2" id="header2" style="display: none">
         <img class="header_left" src="../../../../static/images/discover/backblue.png" @click="goBack">
         <p class="header-title-fff">资讯详情</p>
-        <img class="header_right" src="../../../../static/images/discover/moreblue.png" @click="onShareClick(0)"/>
+        <img class="header_right" src="../../../../static/images/discover/moreblue.png" @click="onShareClick(0)" />
       </header>
       <!--分享组件-->
-      <shareBox :index="0" :item="content" :flag="flag" :type="type" :collectionStatus="content.collectionStatus" :isCenter="true"
-                @closeShare="bgHide" @collection="collection" @reCollection="messageBoxCofirm"></shareBox>
+      <shareBox :index="0" :item="content" :flag="flag" :type="type" :collectionStatus="content.collectionStatus"
+        :isCenter="true" @closeShare="bgHide" @collection="collection" @reCollection="messageBoxCofirm"></shareBox>
       <!--资讯详情S-->
-      <img :src="content.imgUrl" style="width: 100%;" id="bgImg"/>
+      <img :src="content.imgUrl" style="width: 100%;" id="bgImg" />
       <div class="wrap_92">
         <!--内容S-->
         <p class="title_info">
@@ -28,7 +28,7 @@
         </p>
         <div class="contentIconInfo">
           <!--阅读数量-->
-          <img src="../../../../static/images/discover/eye.png" class="f_left"/>
+          <img src="../../../../static/images/discover/eye.png" class="f_left" />
           <span class="f_left">{{content.readNum}}</span>
           <!--是否点赞以及点赞数量-->
           <span class="f_right">{{content.likeNum}}</span>
@@ -44,7 +44,9 @@
             <div class="comment_userinfo">
               <div class="user_head">
                 <div @click="changeUserStartId(item.user.user_id)">
-                  <img v-if="item.user" :src="item.user.head_image" />
+                  <span v-if="item.user && item.user.head_image">
+                    <img :src="item.user.head_image" />
+                  </span>
                   <img v-else src="../../../../static/images/discover/normalhead.png" />
                 </div>
               </div>
@@ -57,8 +59,9 @@
                 </div>
                 <div class="operation_comment">
                   <div>
-                    <img v-if="item.likeStatus" src="../../../../static/images/discover/nozan.png" class="w_04 mr_16 v_m f_left" @click="giveCommentLike(item.id,index)"/>
-                    <img v-else src="../../../../static/images/discover/zan.png" class="w_04 mr_16 v_m f_left" @click="removeCommentLike(item.id,index)"/>
+                    <img v-if="item.likeStatus" src="../../../../static/images/discover/nozan.png" class="w_04 mr_16 v_m f_left"
+                      @click="giveCommentLike(item.id,index)" />
+                    <img v-else src="../../../../static/images/discover/zan.png" class="w_04 mr_16 v_m f_left" @click="removeCommentLike(item.id,index)" />
                     <span style="font-size: 0.28rem;">{{item.likeNum}}</span>
                   </div>
                 </div>
@@ -74,8 +77,8 @@
             <!--评论内容和回复内容S-->
             <div class="comment_content">
               <p @click="commentbtnBack(item.id)">{{item.message}}</p>
-                <div v-if="item.reverts && item.reverts.length>0">
-                  <div class="comment_msg">
+              <div v-if="item.reverts && item.reverts.length>0">
+                <div class="comment_msg">
                   <div v-for="(back,index) in item.reverts.slice(0,3)">
                     <span class="font_1">
                       <span>
@@ -92,7 +95,7 @@
                     </span>
                     <span class="font_2" @click="commentbtnBack(item.id,back.id)">{{back.message}}</span>
                     <span v-if="back.user && userId == back.user.user_id">
-                        <span @click="deleteComment(back.id)" class="font_1">删除</span>
+                      <span @click="deleteComment(back.id)" class="font_1">删除</span>
                     </span>
                   </div>
                   <div class="allHideComment" v-if="item.reverts && item.reverts.length>3" @click="toCommentList(item.id)">
@@ -112,104 +115,128 @@
     <div style="height: 1rem;"></div>
     <!--评论输入框S-->
     <!--<DiscCommentBox ref="commentbox"></DiscCommentBox>-->
-    <div id="commentBg" @click="closeComment"/>
-      <div class="flex contentcenter myInput" id="myInput">
-        <input autofocus="autofocus" ref="commentfocus" id="comment" type="text" v-model="commentMsg" @click="commentbtn" placeholder="写评论..."/>
-        <span class="send" @click="comment">发送</span>
-      </div>
-      <!--评论输入框E-->
+    <div id="commentBg" @click="closeComment" />
+    <div class="flex contentcenter myInput" id="myInput">
+      <input autofocus="autofocus" ref="commentfocus" id="comment" type="text" v-model="commentMsg" @click="commentbtn"
+        placeholder="写评论..." />
+      <span class="send" @click="comment">发送</span>
     </div>
+    <!--评论输入框E-->
+  </div>
 </template>
 
 <script>
   import DiscCommentBox from '../component/DiscCommentBox.vue';
-  import { MessageBox } from 'mint-ui';
-  import { Toast } from 'mint-ui';
+  import {
+    MessageBox
+  } from 'mint-ui';
+  import {
+    Toast
+  } from 'mint-ui';
   import shareBox from '../component/shareBox.vue';
   export default {
     name: "disc-detail",
-    data(){
-      return{
-        listNum:1,
-        manageId:'',
-        content:[],
+    data() {
+      return {
+        listNum: 1,
+        manageId: '',
+        content: [],
         imgTemp: '../../../static/images/1.jpg',
-        conmmentsList:[],
-        fId:null,
-        bId:null,
-        rows:1,
-        commentMsg:'',
+        conmmentsList: [],
+        fId: null,
+        bId: null,
+        rows: 1,
+        commentMsg: '',
         flag: 'information',
         type: 'information',
         userId: this.$store.state.userId,
         isDisable: false,
       }
     },
-    created(){
+    created() {
       this.manageId = this.$route.query.id;
     },
     components: {
       shareBox,
       DiscCommentBox
     },
-    methods:{
-      CommentBoxchange(){
+    methods: {
+      CommentBoxchange() {
         this.$store.dispatch('showcommentBox')
         this.$refs.commentbox.boxfocus();
       },
-      toCommentList: function(id){
-        this.$router.push({path:"/component/commentList",query:{id:id}})
+      toCommentList: function (id) {
+        this.$router.push({
+          path: "/component/commentList",
+          query: {
+            id: id
+          }
+        })
       },
       //资讯收藏
       collection: function () {
         var _this = this;
-        this.$http.post(DISCOVERMESSAGE.informationCollection, {"uid": _this.userId,"lid": _this.manageId}).then(function (res) {
+        this.$http.post(DISCOVERMESSAGE.informationCollection, {
+          "uid": _this.userId,
+          "lid": _this.manageId
+        }).then(function (res) {
           if (res.data.status) {
             _this.content.collectionStatus = false;
-            setTimeout(()=>{
+            setTimeout(() => {
               _this.bgHide();
-            },1500)
+            }, 1500)
           } else {
-            if(_this.$store.state.userId == null){
+            if (_this.$store.state.userId == null) {
               _this.toLogin();
-            }else{
+            } else {
               MessageBox('提示', res.data.errorMsg);
             }
           }
         });
       },
       //资讯取消收藏
-      messageBoxCofirm: function(){
+      messageBoxCofirm: function () {
         //MessageBox.confirm('确定取消收藏?').then(action => {
-          var _this = this;
-          this.$http.post(DISCOVERMESSAGE.informationRemoveCollection, {"uid": _this.userId,"lid": _this.manageId}).then(function (res) {
-            if (res.data.status) {
-              _this.content.collectionStatus = true;
-              setTimeout(()=>{
-                _this.bgHide();
-              },1500)
+        var _this = this;
+        this.$http.post(DISCOVERMESSAGE.informationRemoveCollection, {
+          "uid": _this.userId,
+          "lid": _this.manageId
+        }).then(function (res) {
+          if (res.data.status) {
+            _this.content.collectionStatus = true;
+            setTimeout(() => {
+              _this.bgHide();
+            }, 1500)
+          } else {
+            if (_this.$store.state.userId == null) {
+              _this.toLogin();
             } else {
-              if(_this.$store.state.userId == null){
-                _this.toLogin();
-              }else{
-                MessageBox('提示', res.data.errorMsg);
-              }
+              MessageBox('提示', res.data.errorMsg);
             }
+          }
           //});
         });
       },
-      changeUserStartId(id){
-        if(id == this.$store.state.userId){
-          this.$router.push({path:'/my/mystart'});
-        }else{
+      changeUserStartId(id) {
+        console.log(id == this.$store.state.userId)
+        if (id == this.$store.state.userId) {
+          this.$router.push({
+            path: '/mystart'
+          });
+        } else {
           this.$store.state.UserStartId = id;
-          this.$router.push({path:'/userstart'});
+          this.$router.push({
+            path: '/userstart'
+          });
         }
       },
       //详情
       getDetail: function () {
         var _this = this;
-        this.$http.post(DISCOVERMESSAGE.informationDetail, {"uid": _this.$store.state.userId,"id": _this.manageId}).then(function (res) {
+        this.$http.post(DISCOVERMESSAGE.informationDetail, {
+          "uid": _this.$store.state.userId,
+          "id": _this.manageId
+        }).then(function (res) {
           if (res.data.status) {
             _this.content = res.data.data;
           } else {
@@ -220,14 +247,16 @@
       //删除评论
       deleteComment: function (id) {
         var _this = this;
-        this.$http.post(DISCOVERMESSAGE.deleteComment, {"id": id}).then(function (res) {
+        this.$http.post(DISCOVERMESSAGE.deleteComment, {
+          "id": id
+        }).then(function (res) {
           if (res.data.status) {
             Toast('删除成功');
             _this.getComments();
           } else {
-            if(_this.$store.state.userId == null){
+            if (_this.$store.state.userId == null) {
               _this.toLogin();
-            }else{
+            } else {
               MessageBox('提示', res.data.errorMsg);
             }
           }
@@ -236,14 +265,17 @@
       //给文章点赞
       giveArticleLike: function () {
         var _this = this;
-        this.$http.post(DISCOVERMESSAGE.informationGiveLike, {"uid": _this.$store.state.userId,"lid": _this.manageId}).then(function (res) {
+        this.$http.post(DISCOVERMESSAGE.informationGiveLike, {
+          "uid": _this.$store.state.userId,
+          "lid": _this.manageId
+        }).then(function (res) {
           if (res.data.status) {
             _this.content.likeNum = res.data.data.num;
             _this.content.likeStatus = false;
           } else {
-            if(_this.$store.state.userId == null){
+            if (_this.$store.state.userId == null) {
               _this.toLogin();
-            }else{
+            } else {
               MessageBox('提示', res.data.errorMsg);
             }
           }
@@ -252,46 +284,55 @@
       //给文章取消点赞
       removeArticleLike: function () {
         var _this = this;
-        this.$http.post(DISCOVERMESSAGE.informationRemoveLike, {"uid": _this.$store.state.userId,"lid": _this.manageId}).then(function (res) {
+        this.$http.post(DISCOVERMESSAGE.informationRemoveLike, {
+          "uid": _this.$store.state.userId,
+          "lid": _this.manageId
+        }).then(function (res) {
           if (res.data.status) {
             _this.content.likeNum = res.data.data.num;
             _this.content.likeStatus = true;
           } else {
-            if(_this.$store.state.userId == null){
+            if (_this.$store.state.userId == null) {
               _this.toLogin();
-            }else{
+            } else {
               MessageBox('提示', res.data.errorMsg);
             }
           }
         });
       },
       //给评论点赞
-      giveCommentLike: function (commentsId,index) {
+      giveCommentLike: function (commentsId, index) {
         var _this = this;
-        this.$http.post(DISCOVERMESSAGE.informationCommentGiveLike, {"uid": _this.$store.state.userId,"lid": commentsId}).then(function (res) {
+        this.$http.post(DISCOVERMESSAGE.informationCommentGiveLike, {
+          "uid": _this.$store.state.userId,
+          "lid": commentsId
+        }).then(function (res) {
           if (res.data.status) {
             _this.conmmentsList[index].likeNum = res.data.data.num;
             _this.conmmentsList[index].likeStatus = false;
           } else {
-            if(_this.$store.state.userId == null){
+            if (_this.$store.state.userId == null) {
               _this.toLogin();
-            }else{
+            } else {
               MessageBox('提示', res.data.errorMsg);
             }
           }
         });
       },
       //给评论取消点赞
-      removeCommentLike: function (commentsId,index) {
+      removeCommentLike: function (commentsId, index) {
         var _this = this;
-        this.$http.post(DISCOVERMESSAGE.informationCommentRemoveLike, {"uid": _this.$store.state.userId,"lid":commentsId}).then(function (res) {
+        this.$http.post(DISCOVERMESSAGE.informationCommentRemoveLike, {
+          "uid": _this.$store.state.userId,
+          "lid": commentsId
+        }).then(function (res) {
           if (res.data.status) {
             _this.conmmentsList[index].likeNum = res.data.data.num;
             _this.conmmentsList[index].likeStatus = true;
           } else {
-            if(_this.$store.state.userId == null){
+            if (_this.$store.state.userId == null) {
               _this.toLogin();
-            }else{
+            } else {
               MessageBox('提示', res.data.errorMsg);
             }
           }
@@ -300,7 +341,10 @@
       //浏览量
       getReadNum: function () {
         var _this = this;
-        this.$http.post(DISCOVERMESSAGE.informationRead, {"uid": _this.$store.state.userId,"lid": _this.manageId}).then(function (res) {
+        this.$http.post(DISCOVERMESSAGE.informationRead, {
+          "uid": _this.$store.state.userId,
+          "lid": _this.manageId
+        }).then(function (res) {
           if (res.data.status) {
             _this.content.readNum = res.data.data;
           } else {
@@ -311,7 +355,10 @@
       //获取评论列表
       getComments: function () {
         var _this = this;
-        this.$http.post(DISCOVERMESSAGE.informationCommentList, {"uid": _this.$store.state.userId,"lid": _this.manageId}).then(function (res) {
+        this.$http.post(DISCOVERMESSAGE.informationCommentList, {
+          "uid": _this.$store.state.userId,
+          "lid": _this.manageId
+        }).then(function (res) {
           if (res.data.status) {
             _this.conmmentsList = res.data.data;
           } else {
@@ -322,31 +369,36 @@
       //评论
       comment: function () {
         var _this = this;
-        if(!_this.$store.state.userId){
+        if (!_this.$store.state.userId) {
           _this.toLogin();
           return false;
         }
         _this.isDisable = true;
-        if(_this.msgIsNull(_this.commentMsg)){
+        if (_this.msgIsNull(_this.commentMsg)) {
           MessageBox('提示', "评论内容不能为空");
           _this.isDisable = false
           return false;
         }
         var concatId = null;
-        if(_this.bId){
-          concatId = _this.fId+","+_this.bId;
-        }else{
+        if (_this.bId) {
+          concatId = _this.fId + "," + _this.bId;
+        } else {
           concatId = _this.fId;
         }
-        this.$http.post(DISCOVERMESSAGE.informationComment, {"uid": _this.$store.state.userId,"lid": _this.manageId,"message":_this.commentMsg,"id":concatId}).then(function (res) {
+        this.$http.post(DISCOVERMESSAGE.informationComment, {
+          "uid": _this.$store.state.userId,
+          "lid": _this.manageId,
+          "message": _this.commentMsg,
+          "id": concatId
+        }).then(function (res) {
           if (res.data.status) {
             _this.fId = null;
-            setTimeout(()=>{
+            setTimeout(() => {
               _this.getComments();
               _this.isDisable = false;
-            },1000)
+            }, 1000)
             _this.closeComment();
-            window.scrollTo(0,$("#commentTop").offset().top);
+            window.scrollTo(0, $("#commentTop").offset().top);
           } else {
             MessageBox('提示', "请登录");
             _this.isDisable = false;
@@ -354,26 +406,26 @@
           }
         });
       },
-      msgIsNull: function( str ){
-        if ( str == "" ) return true;
+      msgIsNull: function (str) {
+        if (str == "") return true;
         var regu = "^[ ]+$";
         var re = new RegExp(regu);
         return re.test(str);
       },
       //点击评论
-      commentbtn(){
+      commentbtn() {
         $("#commentBg").show();
         $("#comment").focus();
       },
       //点击回复
-      commentbtnBack(id,backId){
+      commentbtnBack(id, backId) {
         $("#commentBg").show();
         $("#comment").focus();
         this.fId = id;
         this.bId = backId;
       },
       //取消评论
-      closeComment(){
+      closeComment() {
         $("#commentBg").hide();
         $("#id_pl_icon").show();
         this.commentMsg = '';
@@ -381,7 +433,7 @@
       //分享弹出
       onShareClick: function (index) {
         this.indexNum = index;
-        var showId = '#share_information'+index;
+        var showId = '#share_information' + index;
         $(showId).show();
         console.log(showId)
         $("#bgShare").show();
@@ -389,13 +441,13 @@
       },
       //分享关闭
       bgHide: function () {
-        var showId = '#share_information'+this.indexNum;
+        var showId = '#share_information' + this.indexNum;
         $(showId).hide();
         $("#bgShare").hide();
         $("#myInput").show();
       },
       //返回上一级
-      goBack:function () {
+      goBack: function () {
         this.$router.go(-1);
         this.$store.dispatch("showFoot")
       }
@@ -408,29 +460,31 @@
       }
       next();
     },*/
-    mounted(){
+    mounted() {
       this.$store.dispatch("hideFoot"),
         /*悬浮,更换头部背景透明度和文字*/
-        $(window).scroll(()=> {
-          if($("html,body").scrollTop() <= $("#bgImg").height()){
+        $(window).scroll(() => {
+          if ($("html,body").scrollTop() <= $("#bgImg").height()) {
             $("#header1").show();
             $("#header2").hide();
-          }else{
+          } else {
             $("#header1").hide();
             $("#header2").show();
           }
         })
-        //初始化数据
-        this.$nextTick(function () {
-          this.getDetail();
-          this.getReadNum();
-          this.getComments();
-          //this.$store.state.commentboxflag = true;
-        })
+      //初始化数据
+      this.$nextTick(function () {
+        this.getDetail();
+        this.getReadNum();
+        this.getComments();
+        //this.$store.state.commentboxflag = true;
+      })
     }
   }
+
 </script>
 
 <style scoped>
   @import "./../../../../static/css/discover/detail.css";
+
 </style>
