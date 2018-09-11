b2c92d53799fdf5f7331a8d019443adfe5c8247a
fangtao.yu <342285154@qq.com>
2018-09-11 09:52:25 +0800

    我的地址

--- src/components/My/addmydress.vue
+++ src/components/My/addmydress.vue
@@ -16,13 +16,24 @@
                 <span class="contentList-left">手机号码</span>
                 <input type="text" placeholder="点击输入手机号" v-model="num">
             </div>
+            <!-- 省 -->
             <div class="city">
                 <div class="contentList nickname areas">
                     <span class="contentList-left" style="float:left">所在地区</span>
                     <div class="contentList-right" style="float:right">
                       <input type="text" name="" id="" class="place" @click="choosearea()" placeholder="请选择地区" v-model="this.choosedarea">
-                        <!-- <span class="place" @click="choosearea()">{{this.beforechoosedarea}}</span> -->
-                        <img class="pic" src="../../../static/images/my/next@2x.png" />
+                       <img class="pic" src="../../../static/images/my/next@2x.png" />
+                    </div>
+                </div>
+            </div>
+            
+            <!-- 市 -->
+            <div class="city">
+                <div class="contentList nickname areas">
+                    <span class="contentList-left" style="float:left">所在市</span>
+                    <div class="contentList-right" style="float:right">
+                      <input type="text" name="" id="" class="place" @click="choosearea()" placeholder="请选择地区" v-model=" choose_si">
+                       <img class="pic" src="../../../static/images/my/next@2x.png" />
                     </div>
                 </div>
             </div>
@@ -38,10 +49,17 @@
                 <span class="ft_2">&nbsp;设为默认地址</span>
             </div>
         </div>
+        <!-- 省 -->
         <div style="position:absolute;z-index:1000;bottom:0;width:100%;background:#fff;" v-show="this.shows">
-          <div style="text-align:center;line-height:.8rem;font-size:.4rem">选择地区</div>
+          <div style="text-align:center;line-height:.8rem;font-size:.4rem">选择市</div>
           <div style="text-align: right;color: #49BBFF;margin-right:.2rem;" @click="hides()">确定</div>
           <mt-picker :slots="slots" @change="onValuesChange" ></mt-picker>
+        </div>
+        <!-- 市 -->
+        <div style="position:absolute;z-index:1000;bottom:0;width:100%;background:#fff;" v-show="this.shows">
+          <div style="text-align:center;line-height:.8rem;font-size:.4rem">选择市</div>
+          <div style="text-align: right;color: #49BBFF;margin-right:.2rem;" @click="hides()">确定</div>
+          <mt-picker :slots="slots1" @change="onValuesChange1" ></mt-picker>
         </div>
         <div style="height:10rem"></div>
         <span class="bottom-btn" @click="handleSubmit">保存</span>
@@ -59,32 +77,42 @@ export default {
       num: "",
       isShow: false,
       everycode: "", //返回给后台的地区code
+      everyid:'',//通过省份的id  获取对应的市
       allarea: [], //所有的地区
+      city:[],
       choosedarea: "", //被选择的地区
+      choose_si:"",
       ishide: false, //控制城市的显示
       nowindex: 0, //默认显示上海
       provinceId: "100000",
       provinceName: "北京",
       address: "",
       options: [],
-      cityList: ["上海", "北京"],
+      cityList: [], //城市列表
       slots: [
         {
           flex: 1,
           values: [],
-          className: "slot1",
+          className: "slot",
+          textAlign: "center"
+        }
+      ],
+       slots1: [
+        {
+          flex: 1,
+          values: [],
+          className: "slots",
           textAlign: "center"
         }
       ]
     };
   },
   mounted() {
-    // this.getProvinceList();
-    this.info = this.$route.query;
+   this.info = this.$route.query;
     $(".editPersonalDetails").height($(".editPersonalDetails").height());
-    this.$http.post(My.Area, {}).then(res => {
+    this.$http.post(My.Area, {size:1000,parentId: null,level: 1}).then(res => {
       this.allarea = res.data.data.records;
-      for (var i = 0; i < this.allarea.length; i++) {
+     for (var i = 0; i < this.allarea.length; i++) {
         this.slots[0].values.push(this.allarea[i].name);
       }
     });
@@ -185,14 +213,31 @@ export default {
     onValuesChange(picker, values) {
       this.choosedarea = values[0];
       for (var i = 0; i < this.allarea.length; i++) {
-        if (this.allarea[i].name == this.choosedarea) {
+       if (this.allarea[i].name == this.choosedarea) {
           this.everycode = this.allarea[i].code;
+          this.everyid=this.allarea[i].id; 
         }
       }
-      if (values[0] > values[1]) {
-        picker.setSlotValue(1, values[0]);
-      }
     }
+  },
+  watch:{
+   everyid(newVal,oldVal){
+  
+    this.everyid=newVal
+    let data = {
+					parentId: this.everyid, //被检测的省份id 
+					level: 2
+				}
+				this.$http.post(Wit.searchCountryAreaCodeListPage, data, this.$store.state.mytoken).then(res => {
+          const data = res.data;
+        
+					if(data.code == 0) {
+					 this.city = data.data.records; //对应的市
+				  } else {
+					
+					}
+				})
+   }
   }
 };
 // function getData(self, url, param) {
