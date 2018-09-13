d24ca29585e39dd05d01b2438c71f9f0704e8d1a
于成龙 <chenglong.yu@timanetworks.com>
2018-09-14 02:53:09 +0800

    【修改】社区列表显示相互关注的状态

--- src/components/discover/now/now.vue
+++ src/components/discover/now/now.vue
@@ -32,9 +32,13 @@
                       <img src="../../../../static/images/discover/jgz.png">
                       <span class="font_4" @click="addFoucs(item.user.user_id,index)">关注</span>
                     </div>
-                    <div v-else-if="item.focusStatus == 1||item.focusStatus == 1">
+                    <div v-else-if="item.focusStatus == 1">
                       <img src="../../../../static/images/discover/ygz.png">
                       <span class="font_4" @click="removeFoucs(item.user.user_id,index)">已关注</span>
+                    </div>
+                    <div v-else="content.focusStatus == 2">
+                      <img src="../../../../static/images/discover/ygz.png">
+                      <span class="font_4" @click="removeFoucs(content.user.user_id)">相互关注</span>
                     </div>
                   </div>
                 </div>
