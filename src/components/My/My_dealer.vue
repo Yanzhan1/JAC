56992db228020e4655155612593fadb1ec646078
fangtao.yu <342285154@qq.com>
2018-09-14 16:33:43 +0800

    修改

--- src/components/My/My_dealer.vue
+++ src/components/My/My_dealer.vue
@@ -17,7 +17,7 @@
                 <img style="width:100%;height:1.68rem" v-lazy="imgSrc" alt="">
               </div> -->
               <div class="flex column around" style="width:3rem;height:1.5rem">
-                <span style="font-size:.29rem;color:#222;" class="mui-ellipsis-2">{{item.shortDealerName}}</span>
+                <span style="font-size:.29rem;color:#222;" class="mui-ellipsis-2">{{item.dealerName}}</span>
                 <span style="font-size:.23rem;color:#888;">电话：{{item.receiveMessagePhone}}</span>
                 <span class="flex row cocenter">
                   <img style="width:.19rem;height:.22rem;margin-right:.1rem;" src="../../../static/images/Wit/list_position_icon.png" alt="">
