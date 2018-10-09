0667f0251d3d34a5794702bbdca2650e84e2ee2f
dongqin.bao <dongqin.bao@timanetworks.com>
2018-10-09 17:25:36 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -112,7 +112,7 @@ import store from './store'
 //导入jquery
 import $ from 'jquery'
 //手机端调试工具,发布提交时必须注释掉
-import vConsole from './assets/util/vconsole'
+// import vConsole from './assets/util/vconsole'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
