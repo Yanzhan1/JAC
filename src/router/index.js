b760d5838aa1d63a9f1f7ef3e855587d50739d53
David <>
2018-10-18 10:48:51 +0800

    换成大写

--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,7 @@ import WifiSetup from "@/components/Lovecar/WifiSetup" //wifi设置
 import FuelQuery from "@/components/Lovecar/FuelQuery" //燃油查询
 import Datechoose from '@/components/Lovecar/Datechoose' //日期选择
 import FlowQuery from "@/components/Lovecar/FlowQuery" //流量查询
-import FlowBuy from "@/components/lovecar/FlowBuy.vue" //流量购买
+import FlowBuy from "@/components/Lovecar/FlowBuy.vue" //流量购买
 import Islogin from '../components/Lovecar/islogin.vue'
 import IntelligenceParking from "@/components/Lovecar/IntelligenceParking" //智能停车G1
 import IntelligentParkingInfo from "@/components/Lovecar/IntelligentParkingInfo" //智能停车G2
