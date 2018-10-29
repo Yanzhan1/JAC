8c22df53dc1a74845da696bba591e87ee1a2cf86
dongqin.bao <dongqin.bao@timanetworks.com>
2018-10-29 18:44:27 +0800

    修改

--- src/store/state.js
+++ src/store/state.js
@@ -15,6 +15,7 @@ export default {
     refreshToken: '',
     integral: '', //用户总积分
     everyno: '', //智享主页点击时候每个车的no
+    brandName: '', //爱车页面默认车辆的车名
     seriesName: '', //智享主页点击时候车的每一个seriesName
     srouceNo: '', //智享主页点击时候车的每一个srouceNo
     // userNo: 'AD022018081504171568405', //userNo
