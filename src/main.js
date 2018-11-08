b22211155c7c32f8a7d91569d4e5985615ae0f62
于成龙 <chenglong.yu@timanetworks.com>
2018-11-08 14:57:50 +0800

    【修改】尝试修复android 4.4功能

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import 'babel-polyfill'
-import Vue from 'vue'
-import App from './App'
-
 import Es6Promise from 'es6-promise'
 require('es6-promise').polyfill()
 Es6Promise.polyfill()
+
+import Vue from 'vue'
+import App from './App'
+
+
 
 //导入mint-ui
 import mintui from 'mint-ui'
