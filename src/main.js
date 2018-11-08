2c3e9bbd7bf4c6e4738dd78bf1d6529c80b85141
David <>
2018-11-08 13:41:14 +0800

    白屏 试一试

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
-import 'babel-polyfill'
-import Es6Promise from 'es6-promise'
-require('es6-promise').polyfill()
-Es6Promise.polyfill()
-
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 
+import Es6Promise from 'es6-promise'
+require('es6-promise').polyfill()
+Es6Promise.polyfill()
 
 //导入mint-ui
 import mintui from 'mint-ui'
