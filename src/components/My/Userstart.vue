c787345a383e3fee741176ee6a707cca960ac615
Xu Tingting <tingting.xu@timanetworks.com>
2018-12-18 16:09:26 +0800

    改bug

--- src/components/My/Userstart.vue
+++ src/components/My/Userstart.vue
@@ -455,7 +455,7 @@
         var _this = this;
         this.$http.post(DISCOVERMESSAGE.issueMomentList, {
           // "uid": _this.$store.state.userId,
-          "uid": _this.$route.query.id,
+          "uid": _this.$store.state.userId,
           "hisUid": _this.$store.state.UserStartId
         }).then(function (res) {
           if (res.data.status) {
