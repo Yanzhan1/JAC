55d0972b78befb64a010c718777f09fab0015dcb
dongqin.bao <dongqin.bao@timanetworks.com>
2019-03-04 16:34:39 +0800

    增加维保预约行驶里程的限制,加单位km,维修保养的ui修改

--- src/components/Wit/pre_weib.vue
+++ src/components/Wit/pre_weib.vue
@@ -49,7 +49,7 @@
 				<div class="flex row cocenter">
           <!-- <input type="numberbox" name="" id="" v-model="km"> -->
           <input type="number"
-              placeholder="请输入数字" v-model="km">
+              placeholder="请输入数字" v-model="km">&nbsp km
 					<img src="../../../static/images/next@2x.png" alt="" style="width:.16rem;height:.3rem">
 				</div>
 			</div>
