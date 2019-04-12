1698cba356c9afac40657ffb636be02b839f13e4
dongqin.bao <dongqin.bao@timanetworks.com>
2019-04-12 10:23:47 +0800

    11

--- static/js/interfaceUrl.js
+++ static/js/interfaceUrl.js
@@ -6,6 +6,9 @@
 // //测试环境域名
 var host = '//test.jac.timanetwork.net'
 
+// //江淮内网测试环境域名
+// var host = '//jacsupperapptest.jac.com.cn'
+
 //云环境域名
 // var host = 'https://jacsupperappuat.jac.com.cn/'
 
@@ -18,14 +21,14 @@ var focusip = host + '/api/dk-pr-svr' //uat
 var point = 'http://192.168.1.141:8868' //埋点
     //服务器-智享
 var mip = host + '/api/jac-automobile-manage/automobilemanage' //智享
-var witlocal = host + '/api/jacenergy' //杨毅飞本地服务器
+var witlocal = host + '/api/jac-energy/jacenergy' //线上
     // var witlocal = 'http://172.21.12.74:8086/jacenergy' //杨毅飞本地服务器
     //服务器-admin(跟用户有关即我的)
 var mips = host + '/api/jac-admin' //地区
 var jf = host + '/' //跟积分有关的
     // var love_car = host+'/jvconnectedcar' //爱车
 var love_car = host + '/api/jac-car-control' //爱车
-    // var love_car = 'http://172.21.12.74:8086' //爱车
+    // var love_car = 'http://172.21.12.74:8084' //爱车
     //服务器→反馈记录
 var rec = host + '/api/jac-enjoy-service/'
     // H5外网地址，分享用
