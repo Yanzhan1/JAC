84691759ae2d48c28afda2858987ef565704fe8e
dongqin.bao <dongqin.bao@timanetworks.com>
2019-06-11 16:25:07 +0800

    轻卡主入口

--- src/components/Lovecar/lightcar/lightMain.vue
+++ src/components/Lovecar/lightcar/lightMain.vue
@@ -1,12 +1,29 @@
 <template>
       <div>
-          主入口
       </div>
 </template>
 
 <script>
 export default {
+    data(){
+      return{
 
+      }
+    },
+    methods:{
+      onloaded(){
+        window.location.hash.split('?')[1]
+        if(window.location.hash.split('?')[1]=='traffic'){
+            this.$router.push("/Lovecar/FlowQuery");
+        }else if(window.location.hash.split('?')[1]=='diagnosis'){
+            this.$router.push("/Bus_test");
+        }
+      }
+    },
+    created(){
+      window.onloaded=this.onloaded
+
+    }
 }
 </script>
 
